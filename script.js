c195639ed6f3afe6f9c0e5e715576a5ca07cd19c
Iswarya-S26 <emails2iswarya2602@gmail.com>
2024-12-26 21:29:58 +0530

    finish displaying balances

--- script.js
+++ script.js
@@ -4,19 +4,30 @@ const descriptionElement = document.getElementById("description-input");
 const amountElement = document.getElementById("amount-input");
 const addBtn = document.getElementById("btn-add");
 const tbodyOutput = document.getElementById("transaction-tbody");
+//getting balances element
+const total_element = document.getElementById("total-ouput");
+const income_element = document.getElementById("income-output");
+const expense_element = document.getElementById("expense-output");
 
+// global transation array
 let transactionArray = [];
+let total_balance = 0;
+let total_income = 0;
+let total_expense = 0;
+
 addBtn.addEventListener("click", (e) => {
   e.preventDefault();
   addTransaction();
 });
 
+//clearing input value
 function clearAll() {
   transSelectElement.value = "";
   descriptionElement.value = "";
   amountElement.value = "";
 }
 
+//add transaction
 function addTransaction() {
   if (
     transSelectElement.value == "" ||
@@ -38,7 +49,7 @@ function addTransaction() {
   loadTransaction();
 }
 
-//{id: 1735223150895, trans_type: 'income', trans_description: 'salary', trans_amount: '1000'}
+//load transaction
 function loadTransaction() {
   let output = "";
   transactionArray.forEach((item, index) => {
@@ -55,6 +66,7 @@ function loadTransaction() {
         </tr>
         `;
   });
+  //fetching all delete buttons
   tbodyOutput.innerHTML = output;
   const deleteBtns = document.querySelectorAll(".delete-btn");
   console.log(deleteBtns);
@@ -65,11 +77,31 @@ function loadTransaction() {
       deleteTransaction(id);
     });
   });
+  displayBalance();
 }
+
+//{id: 1735223150895, trans_type: 'income', trans_description: 'salary', trans_amount: '1000'}
+function displayBalance() {
+  total_income = 0;
+  total_expense = 0;
+  transactionArray.forEach((item) => {
+    if (item.trans_type == "income") {
+      total_income += Number(item.trans_amount);
+    } else {
+      total_expense += Number(item.trans_amount);
+    }
+  });
+  total_balance = total_income - total_expense;
+  income_element.innerHTML = total_income.toFixed(2);
+  expense_element.innerHTML = total_expense.toFixed(2);
+  total_element.innerHTML = total_balance.toFixed(2);
+}
+
+//delete transcation
 function deleteTransaction(id) {
   if (confirm("Are You Sure To Delete?")) {
-    let delete_trans = transactionArray.filter((item) => item.id != id);
-    transactionArray = delete_trans;
+    let updated_array = transactionArray.filter((item) => item.id != id);
+    transactionArray = updated_array;
     loadTransaction();
   }
 }
